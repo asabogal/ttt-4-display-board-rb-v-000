1b8acf612c35d27eb6c55511046242df1100d5cb
Alejandro Sabogal <asabogal.git@gmail.com>
2018-03-17 00:16:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,16 +1,10 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-  # puts "   |   |   "
-  # puts "-----------"
-  # puts "   |   |   "
-  # puts "-----------"
-  # puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[3]} "
+  puts "-----------"
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
+  puts "-----------"
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
 
 end
-
-# puts " #{board[0]} | #{board[1]} | #{board[3]} "
-# puts "-----------"
-# puts " #{board[4]} | #{board[5]} | #{board[6]} "
-# puts "-----------"
-# puts " #{board[7]} | #{board[8]} | #{board[9]} "
